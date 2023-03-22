c72cfcf22930714415f22c9bf750e22e221a391b
Manju2012 <naganurprasad@yahoo.in>
2023-03-22 13:35:59 +0530

    add mult func

--- calculator.py
+++ calculator.py
@@ -9,3 +9,7 @@ def add(first_term, second_term):
 
 def sub(first_term, second_term):
     return first_term - second_term
+
+
+def mul(firstt_term, second_term):
+    return firstt_term * second_term
